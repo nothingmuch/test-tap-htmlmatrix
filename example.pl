1c7fb2484f9208db3a0b6cfa86f7a912531b24b1
Yuval Kogman <nothingmuch@woobling.org>
2006-03-27 18:43:20 +0000

    update example

--- example.pl
+++ example.pl
@@ -7,25 +7,25 @@ use Test::TAP::HTMLMatrix;
 use Test::TAP::Model::Visual;
 use Test::TAP::Model::Consolidated;
 
+# make a successful test run
 my $model_ok = Test::TAP::Model::Visual->new_with_tests(glob("t/*.t"));
 $model_ok->desc_string("real run");
+
+# make a dummy run with some failures
 my $model_failing = do { local $ENV{TEST_FAIL_RANDOMLY} = 1; Test::TAP::Model::Visual->new_with_tests(glob("t/*.t")) };
 $model_failing->desc_string("dummy failures");
 
-#open STDOUT, ">", "example.html"; #"|xmllint --noblanks -> example.html" or die "couldn't pipe to xmllint";
-
 my $v = Test::TAP::HTMLMatrix->new(Test::TAP::Model::Consolidated->new($model_ok, $model_failing));
 
-my $t = times;
-
+# control the HTML output
 $v->has_inline_css(1);
 #$v->no_javascript(1);
 
-$v->output_dir("output/");
+# this is the most popular view:
+print $v->detail_html;
+
+# you can also get a more compact summary:
 #print $v->summary_html;
-#print $v->detail_html;
 
-print STDERR "open example.html\n";
-
-print STDERR "Generating HTML took" . (times() - $t) . "\n";
-
+# or a directory with both files and some css
+# $v->output_dir("example_html");
